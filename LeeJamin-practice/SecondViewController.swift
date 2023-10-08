178e76c8bc636e47d7d0e1604d61f60f4fb59166
jaminleee <jamin_00@naver.com>
2023-10-09 02:15:02 +0900

    practice

---
+++ LeeJamin-practice/SecondViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SecondViewController.swift
+//  LeeJamin-practice
+//
+//  Created by 이자민 on 2023/10/07.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    var email: String = ""
+    var password: String = ""
+    var delegate: GetDataProtocol?
+    var loginDataCompletion: (([String]) -> Void)? //스트링 타입의 배열을 매개변수로 전달 할거고, 리턴타입은 void다!
+    
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    private func bindText() {
+        self.emailLabel.text = "email : \(email)"
+        self.passwordLabel.text = "password : \(password)"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindText()
+        backButton.layer.cornerRadius = 5
+
+    }
+    
+    @IBAction func backButtonTap(_ sender: Any) {
+        if self.navigationController != nil {
+            self.navigationController?.popViewController(animated: true)
+        }else{
+            self.dismiss(animated: false)
+        }
+        
+        delegate?.getLoginData(email: email, password: password)
+        
+        guard let loginDataCompletion else {return}
+                loginDataCompletion([self.email, self.password])
+    }
+    
+}
