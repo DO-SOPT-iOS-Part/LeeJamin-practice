1e4d3f2a16ceea2e533e233e9b59396e44dda68b
jaminleee <jamin_00@naver.com>
2023-10-10 18:30:04 +0900

    1주차과제

---
+++ LeeJamin-practice/GetDataProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  GetDataProtocol.swift
+//  LeeJamin-practice
+//
+//  Created by 이자민 on 2023/10/07.
+//
+
+import Foundation
+
+protocol GetDataProtocol {
+    func getLoginData(email: String, password: String)
+}
--- LeeJamin-practice/ViewController.swift
+++ LeeJamin-practice/ViewController.swift
@@ -8,12 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var idText: String = ""
+    private var passwordText: String = ""
 
+    @IBOutlet weak var idTextFieldDidEditing: UITextField!
+    @IBOutlet weak var passwordTextFieldDidEditing: UITextField!
+    @IBOutlet weak var passwordLookButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 8
     }
 
+    @IBAction func idTextFieldDidEditing(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+                if let idText = textField.text {
+                    self.idText = idText
+                }
+    }
+    
 
+    @IBAction func passwordTextFieldDidEditing(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let passwordText = textField.text {
+            self.passwordText = passwordText
+        }
+    }
+    @IBAction func passwordLookButtonTap(_ sender: Any) {
+        
+        passwordTextFieldDidEditing.isSecureTextEntry.toggle()
+        passwordLookButton.isSelected.toggle()
+        if passwordLookButton.isSelected{
+            passwordLookButton.setImage(UIImage(systemName: "eye"), for: .normal)
+        }
+        else {
+            passwordLookButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        }
+    }
+    
+    @IBAction func loginButtonTap(_ sender: UIButton) {
+    
+        print("\(idText)\n\(passwordText)")
+        pushToSecondViewController()
+    }
+    
+    
+    func pushToSecondViewController() {
+        guard let SecondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else {return}
+            
+        SecondViewController.email = idText
+        SecondViewController.password = passwordText
+        
+        SecondViewController.delegate = self
+        
+        
+        
+        //self.present(SecondViewController,animated: true)
+        self.navigationController?.pushViewController(SecondViewController, animated: true)
+        
+        SecondViewController.loginDataCompletion = { data in
+                    print("클로저로 받아온 email : \(data[0]), 클로저로 받아온 password : \(data[1])")
+                }
+    }
 }
 
+extension ViewController: GetDataProtocol {
+    func getLoginData(email: String, password: String) {
+        print("받아온 email : \(email), 받아온 password : \(password)")
+    }
+}
